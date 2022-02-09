96a48a960b65607bf46999d568d913aa43375904
Maxime Nguyen <75279829+Squalord@users.noreply.github.com>
2022-02-09 14:03:07 +0100

    PokeApp ListView

---
+++ PokeApp/Models/Pokemons.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokeApp.Models
+{
+    public class Pokemons
+    {
+        public int id;
+        public string name;
+        public string type;
+        public string image;
+
+        public int Id
+        {
+            get => id;
+            set => id = value;
+        }
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public string Type
+        {
+            get => type;
+            set => type = value;
+        }
+
+        public string Image
+        {
+            get => image;
+            set => image = value;
+        }
+    }
+}
---
+++ PokeApp/ViewModels/PokemonsVM.cs
@@ -0,0 +1,31 @@
+﻿using PokeApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace PokeApp.ViewModels
+{
+    public class PokemonsVM
+    {
+        private ObservableCollection<Pokemons> myList;
+
+        public ObservableCollection<Pokemons> MyList
+        {
+            get { return myList; }
+            set { myList = value; }
+        }
+
+        public PokemonsVM()
+        {
+            MyList = new ObservableCollection<Pokemons>() {
+                new Pokemons()
+                {
+                    Id = 1,
+                    Name = "Diancie",
+                    Type = "Normal"
+                },
+            };
+        }
+    }
+}
